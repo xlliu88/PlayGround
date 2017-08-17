42d790751c5ceda684365ba936334c101c23faf5
xlliu88 <xlliu88@gmail.com>
2017-08-16 20:59:45 -0500

    dotnet practice: add console_1 and console_2

---
+++ console_2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace console_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello HaHaHa DEV275x!");
+        }
+    }
+}
